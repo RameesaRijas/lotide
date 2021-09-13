e28fa37a1ceb5136011ad0015e9852f64e4adccb
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-13 18:00:06 -0400

    added comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  let cross = String.fromCodePoint(10060);
-  let tick = String.fromCodePoint(9989);
+  let cross = String.fromCodePoint(10060);//cross emoji
+  let tick = String.fromCodePoint(9989);//tick emoji
   if (actual === expected) {
     console.log(`${tick}${tick} Assertion Passed: ${actual} === ${expected}`);
   } else {
