d6a36de32c4c299b173ef3d37ccb955060c62ef3
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-13 16:53:29 -0400

    implemented assert equal

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let cross = String.fromCodePoint(10060);
+  let tick = String.fromCodePoint(9989);
+  if (actual === expected) {
+    console.log(tick + tick + " Assertion Passed: " + `${actual}` + " === " + `${expected}`);
+  } else {
+    console.log(cross + cross + " Assertion Failed: " + `${actual}` + " === " + `${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
