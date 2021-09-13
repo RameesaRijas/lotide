5f84951573ba385fa458744514f23b343d29bfcb
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-13 17:12:07 -0400

    implemented assert equal-small used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`${tick}${tick} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${cross}${cross} Assertion Failed: ${ actual} !== ${expected}`);
+    console.log(`${cross}${cross} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
