735ff835a582193ccaf5735b4fc3387bf11991ac
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-14 19:23:05 -0400

    linted code

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,16 +1,16 @@
 //getting input arrays
 const eqArrays = (arrayOne , arrayTwo) => {
-  if(arrayOne.length !== arrayTwo.length) {
+  if (arrayOne.length !== arrayTwo.length) {
     return false;
-  } 
+  }
   //checking if array is matched or else return false
-  for(let i = 0; i < arrayOne.length; i++) {
-    if(arrayOne[i] !== arrayTwo[i]) {
-      return false ;
-    } 
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
   }
   return true;
-}
+};
 
 const assertArraysEqual = (arr1, arr2) => {
   let cross = String.fromCodePoint(10060);//cross emoji
@@ -21,7 +21,7 @@ const assertArraysEqual = (arr1, arr2) => {
   } else {
     console.log(`${cross}${cross} Assertion Failed`);
   }
-}
+};
 
 assertArraysEqual([1, 2, 2], [1, 2, 3]);
 
