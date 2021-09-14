0cc66a7e2d957d5e03912ff368b4098defc57e3b
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-14 19:05:01 -0400

    assertArrayequal function

---
+++ assertArrayEqual.js
@@ -0,0 +1,27 @@
+//getting input arrays
+const eqArrays = (arrayOne , arrayTwo) => {
+  if(arrayOne.length !== arrayTwo.length) {
+    return false;
+  } 
+  //checking if array is matched or else return false
+  for(let i = 0; i < arrayOne.length; i++) {
+    if(arrayOne[i] !== arrayTwo[i]) {
+      return false ;
+    } 
+  }
+  return true;
+}
+
+const assertArraysEqual = (arr1, arr2) => {
+  let cross = String.fromCodePoint(10060);//cross emoji
+  let tick = String.fromCodePoint(9989);//tick emoji
+  let result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`${tick}${tick} Assertion Passed`);
+  } else {
+    console.log(`${cross}${cross} Assertion Failed`);
+  }
+}
+
+assertArraysEqual([1, 2, 2], [1, 2, 3]);
+
