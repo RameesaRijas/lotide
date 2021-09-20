4df3f940bc3d9934d320f1945ab571a0e66b3725
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-20 14:17:56 -0400

    refactor middle

--- middle.js
+++ middle.js
@@ -1,29 +1,4 @@
 //getting input arrays
-const eqArrays = (arrayOne , arrayTwo) => {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  //checking if array is matched or else return false
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  let cross = String.fromCodePoint(10060);//cross emoji
-  let tick = String.fromCodePoint(9989);//tick emoji
-  let result = eqArrays(arr1, arr2);
-  if (result) {
-    console.log(`${tick}${tick} Assertion Passed`);
-  } else {
-    console.log(`${cross}${cross} Assertion Failed`);
-  }
-};
-
-
 const middle = (arrayOfElement) => {
   let middleElementArray = [];
   if (arrayOfElement.length > 2) {
@@ -38,10 +13,4 @@ const middle = (arrayOfElement) => {
   return middleElementArray;
 };
 
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]) , []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]) , [3, 4]);
+module.exports = middle;
